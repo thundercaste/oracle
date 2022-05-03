3d7e48cb1e5dc0a4f605fc8b7b168f123bc68e2e
ThunderCaste <caz@MacBook-Pro-6.local>
2022-05-03 13:36:18 -0700

    Revamped website to mimic weather dot com

---
+++ script.js
@@ -0,0 +1,17 @@
+let elementary = 'grades k-5'
+let middle = 'grades 6-8' 
+let high = 'grades 9-12'
+let num = 1
+
+let work = num + 1
+
+const schoolgrade = [elementary, middle, high]
+const myFirstObject = {
+    firstName:"pitor",
+    lastName:"hulk",
+    age: 56,
+    eyeColor: schoolgrade
+}
+
+console.log(elementary);
+console.log(middle);
